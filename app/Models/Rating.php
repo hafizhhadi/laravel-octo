1a552a9a4b91fab3cc0f23a366b8081dbc74b6d8
Hafiz Hadi <hafizhadi581@gmail.com>
2023-07-18 12:50:50 +0800

    feat: rating model  and relation

---
+++ app/Models/Rating.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+        'rating',
+        'description'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class, 'movie_id');
+    }
+}
