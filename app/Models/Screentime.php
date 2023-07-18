6f46bcdd8782698908c8f26a3fa9597cae28597f
Hafiz Hadi <hafizhadi581@gmail.com>
2023-07-18 12:50:57 +0800

    feat: screentime model  and relation

---
+++ app/Models/Screentime.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Movie;
+use App\Models\Theater;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Screentime extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'movie_id',
+        'theater_id',
+        'start_date_time',
+        'end_date_time'
+    ];
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class, 'movie_id');
+    }
+
+    public function theater()
+    {
+        return $this->belongsTo(Theater::class);
+    }
+}
