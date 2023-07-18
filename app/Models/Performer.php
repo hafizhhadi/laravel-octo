8bf853fa653dd8c2c9a6e5196fd6118423f09d00
Hafiz Hadi <hafizhadi581@gmail.com>
2023-07-18 12:50:45 +0800

    feat: performer model  and relation

---
+++ app/Models/Performer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Performer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function movies()
+    {
+        return $this->belongsToMany(Movie::class, 'movie_performer', 'performer_id', 'movie_id');
+    }
+}
